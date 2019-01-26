f67a145bed1881c7f92c51d18e7ce009b121dee6
Abstract Creative <abstract@Abstracts-MacBook-Air.local>
2019-01-26 13:29:34 -0600

    removed books model and controllers

--- controllers/vinControllers.js
+++ controllers/vinControllers.js
@@ -1,31 +1,31 @@
 const db = require("../models");
 
-// Defining methods for the bookController
+// Defining methods for the CarController
 module.exports = {
   findAll: function(req, res) {
-    db.Book.find(req.query)
-      .then(dbBook => res.json(dbBook))
+    db.Car.find(req.query)
+      .then(dbCar => res.json(dbCar))
       .catch(err => res.status(422).json(err));
   },
   findById: function(req, res) {
-    db.Book.findById(req.params.id)
-      .then(dbBook => res.json(dbBook))
+    db.Car.findById(req.params.id)
+      .then(dbCar => res.json(dbCar))
       .catch(err => res.status(422).json(err));
   },
   create: function(req, res) {
-    db.Book.create(req.body)
-      .then(dbBook => res.json(dbBook))
+    db.Car.create(req.body)
+      .then(dbCar => res.json(dbCar))
       .catch(err => res.status(422).json(err));
   },
   update: function(req, res) {
-    db.Book.findOneAndUpdate({ id: req.params.id }, req.body)
-      .then(dbBook => res.json(dbBook))
+    db.Car.findOneAndUpdate({ id: req.params.id }, req.body)
+      .then(dbCar => res.json(dbCar))
       .catch(err => res.status(422).json(err));
   },
   remove: function(req, res) {
-    db.Book.findById(req.params.id)
-      .then(dbBook => dbBook.remove())
-      .then(dbBook => res.json(dbBook))
+    db.Car.findById(req.params.id)
+      .then(dbCar => dbCar.remove())
+      .then(dbCar => res.json(dbCar))
       .catch(err => res.status(422).json(err));
   }
 };
