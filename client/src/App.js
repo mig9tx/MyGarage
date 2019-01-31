fbfe078f8dd510c7035b0b23cef165321f2676c7
Abstract Creative <abstract@Abstracts-MacBook-Air.local>
2019-01-30 20:57:41 -0600

    new login details

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,63 @@
-import React from "react";
+import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Home from "./pages/Home";
-import Saved from "./pages/Saved";
-import NoMatch from "./pages/NoMatch";
-import Nav from "./components/Nav";
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./utils/setAuthToken";
 
-function App() {
+// Private Route
+import PrivateRoute from "./components/private-route/PrivateRoute";
+import { setCurrentUser, logoutUser } from "./actions/authActions";
+import { Provider } from "react-redux";
+import store from "./store";
+
+import Nav from "./components/Nav";
+import Home from "./pages/Home";
+import Landing from "./pages/Landing";
+import Saved from "./pages/Saved";
+// import NoMatch from "./pages/NoMatch";
+import Register from "./pages/auth/Register";
+import Login from "./pages/auth/Login";
+
+// Check for token to keep user logged in
+if (localStorage.jwtToken) {
+  // Set auth token header auth
+  const token = localStorage.jwtToken;
+  setAuthToken(token);
+  // Decode token and get user info and exp
+  const decoded = jwt_decode(token);
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+  // Check for expired token
+  const currentTime = Date.now() / 1000; // to get in milliseconds
+  if (decoded.exp < currentTime) {
+    // Logout user
+    store.dispatch(logoutUser());
+
+    // Redirect to login
+    window.location.href = "./login";
+  }
+}
+
+class App extends Component {
+  render() {
   return (
+    <Provider store={store}>
     <Router>
       <div>
         <Nav />
-        <Switch>
-          <Route exact path="/" component={Home} />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+          {/* <Route component={NoMatch} /> */}
+
+          <Switch>
+          <PrivateRoute exact path="/home" component={Home} />
           <Route exact path="/saved" component={Saved} />
-          <Route component={NoMatch} />
-        </Switch>
+          </Switch>
       </div>
-    </Router>
+      </Router>
+      </Provider>
   );
+  }
 }
 
 export default App;
