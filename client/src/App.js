2c8fb8d491247e31ff5ef891b759df44baa1fced
Efrencav <efren.cavazos@gmail.com>
2019-02-03 16:48:03 -0600

    fixed files

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import jwt_decode from "jwt-decode";
-import setAuthToken from "./utils/setAuthToken";
+// import jwt_decode from "jwt-decode";
+// import setAuthToken from "./utils/setAuthToken";
 // import { Nav } from "reactstrap";
 
 
