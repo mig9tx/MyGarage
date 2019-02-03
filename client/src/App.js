2ec975895b491ffeb7bfac0b5e19df0fe189656d
mig9tx <migue.trujillo@utexas.edu>
2019-02-02 21:20:14 -0600

    maintenance added to the correct object. not saved to database though.

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import Home from "./pages/Home";
 import Saved from "./pages/Saved";
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
-import Maintenance from "./pages/Maintenance"
+import Maintenance from "./pages/Maintenance";
 
 function App() {
   return (
